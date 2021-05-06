4a53b659e966eb98a0cbbfbd52af6e54b2c99f36
garrathjones <jones.garrath@gmail.com>
2021-05-06 21:57:36 +0100

    Initial commit

---
+++ SeleniumCSharpNetCore/UnitTest1.cs
@@ -0,0 +1,42 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace SeleniumCSharpNetCore
+{
+    public class Tests : DriverHelper
+    {
+
+        
+        [SetUp]
+        public void Setup()
+        {
+            Console.WriteLine("Setup");
+            //ChromeOptions chromeOptions = new ChromeOptions();
+            Driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            
+            Driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com");
+            Driver.FindElement(By.CssSelector(".o-ochk > ul:nth-child(1) > li:nth-child(1) > label:nth-child(1) > div:nth-child(3)")).Click();
+            Driver.FindElement(By.Id("ContentPlaceHolder1_Meal")).SendKeys("Tomato");
+
+            string comboBoxControlName = "ContentPlaceHolder1_AllMealsCombo";
+
+            CustomControl1.ComboBox(comboBoxControlName,"Almonds");
+
+
+
+
+
+
+
+            Console.WriteLine("Test1");
+            Assert.Pass();
+        }
+    }
+}
