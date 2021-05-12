771f0c6f846081eba87a18c624222344b77bf7e7
garrathjones <jones.garrath@gmail.com>
2021-05-12 22:55:32 +0100

    Began adding SpecFlow test

---
+++ SeleniumCSharpNetCore/Steps/LoginSteps.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace SeleniumCSharpNetCore.Steps
+{
+    [Binding]
+    public class LoginSteps
+    {
+        [When(@"the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
